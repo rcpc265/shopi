531830c7741cd4db0b256657acbe44f33aa63783
Rafael Cangahuala <78826890+rcpc265@users.noreply.github.com>
2024-02-04 15:54:52 -0500

    Set up React Query for data fetching

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
+import Providers from "./Providers";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
-        <main className="my-20 flex flex-col items-center">{children}</main>
+        <Providers>
+          <NavBar />
+          <main className="my-20 flex flex-col items-center">{children}</main>
+        </Providers>
       </body>
     </html>
   );
